957b2ec6b2b2850af770d0d149a7587e3619f7ec
Antibiotic <ofsposs@gmail.com>
2023-12-22 14:40:51 +0100

    mongo+config

---
+++ src/conf/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+
+class Settings(BaseSettings):
+    username_mongo: str = Field(..., env="USERNAME_MONGO") 
+    password_mongo: str = Field(..., env="PASSWORD_MONGO") 
+
+    class Config:
+        env_file = '.env.sample'   
+        env_file_encoding = 'utf-8' 
+
+
+settings = Settings()
