1062beac517cab4fa9268394e7ff0bdc644f64be
Anastasiia Mishchenko <mishenko971110@gmail.com>
2023-12-24 11:05:00 +0200

    add-chat-history

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, UploadFile, File, HTTPException
 from src.schemas import HealthCheck
+from fastapi.responses import HTMLResponse
+from typing import List
+import openai
 
 app = FastAPI()
-
-
+# Встановлення вашого API ключа
+api_key = "YOUR_API_KEY"
+openai.api_key = api_key
 
 @app.get(
     "/health",
@@ -25,3 +29,34 @@ def get_health() -> HealthCheck:
     """
     return HealthCheck(status="OK")
 
+history = []
+
+# Функція для взаємодії з Large Language Model
+def generate_response_from_model(message: str) -> str:
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Ви: " + message},
+            {"role": "user", "content": ""},
+        ],
+    )
+    reply = response['choices'][0]['message']['content']
+    return reply
+
+
+@app.post("/chat/")
+async def chat_with_model(message: str):
+    global history
+    history.append(message)
+    response = generate_response_from_model(message)
+    return {"response": response}
+
+
+@app.get("/chat_history/", response_class=HTMLResponse)
+async def show_chat_history():
+    global history
+    html_content = "<h2>Історія чату:</h2>"
+    for idx, message in enumerate(history, start=1):
+        html_content += f"<p><strong>Повідомлення {idx}:</strong> {message}</p>"
+
+    return html_content
