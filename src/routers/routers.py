8825a6cac203e94e9889a6853177ec5ed49b3af2
Igor Omelchenko <110295552+Derangedwerwolf@users.noreply.github.com>
2024-01-08 16:45:27 +0200

    python string

--- src/routers/routers.py
+++ src/routers/routers.py
@@ -1,12 +1,22 @@
-from fastapi import APIRouter, Depends,  HTTPException, status, Request
+from fastapi import APIRouter, Depends, HTTPException, status, Request
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import RedirectResponse as redirect
 
 from src.schemas import UserModel, MessageModel
 from src.templates.auth import Hash, create_access_token, get_current_user
-from src.templates.operations import create_user, get_db, get_payment, execute_paypal_payment
+from src.templates.operations import (
+    create_user,
+    get_db,
+    get_payment,
+    execute_paypal_payment,
+)
 from src.templates.operations import create_chat, create_message, get_chat_history
-from src.templates.operations import size_warning_response, extract_text_from_pdf, extract_data_from_csv, extract_text_from_docx
+from src.templates.operations import (
+    size_warning_response,
+    extract_text_from_pdf,
+    extract_data_from_csv,
+    extract_text_from_docx,
+)
 
 from starlette.responses import FileResponse
 
@@ -18,20 +28,20 @@ from docx import Document
 
 from src.utils.get_mongo import get_mongodb
 
-main = APIRouter(prefix='/main', tags=['main'])
-users = APIRouter(prefix='/users', tags=['users'])
-chats = APIRouter(prefix='/chats', tags=['chats'])
+main = APIRouter(prefix="/main", tags=["main"])
+users = APIRouter(prefix="/users", tags=["users"])
+chats = APIRouter(prefix="/chats", tags=["chats"])
 
 # router = APIRouter(prefix="/pdf", tags=["pdf"])
 router = APIRouter(prefix="/files", tags=["files"])
 
 
-payment = APIRouter(prefix='/payment', tags=['payment'])
+payment = APIRouter(prefix="/payment", tags=["payment"])
 
 hash_handler = Hash()
 
 
-@users.post('/signup', status_code=status.HTTP_201_CREATED)
+@users.post("/signup", status_code=status.HTTP_201_CREATED)
 async def signup(user: UserModel):
     """
     User registration.
@@ -56,19 +66,25 @@ async def login(body: OAuth2PasswordRequestForm = Depends()):
     try:
         collection = get_db()
         try:
-            user = await collection.find_one({'username': body.username})
+            user = await collection.find_one({"username": body.username})
         except Exception:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username")
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username"
+            )
         if user is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username")
-        if not hash_handler.verify_password(body.password, user['password']):
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username"
+            )
+        if not hash_handler.verify_password(body.password, user["password"]):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password"
+            )
 
-        access_token = await create_access_token(data={"sub": user['username']})
+        access_token = await create_access_token(data={"sub": user["username"]})
         return {"access_token": access_token, "token_type": "bearer"}
     except Exception as e:
         print(e)
-        raise HTTPException(status_code=400, detail='False')
+        raise HTTPException(status_code=400, detail="False")
 
 
 @users.get("/close-root-for-test")
@@ -92,14 +108,16 @@ async def create_chat_route(current_user: dict = Depends(get_current_user)):
         dict: A message indicating successful creation along with the chat ID.
     """
     try:
-        chat_id = await create_chat(current_user['_id'])
+        chat_id = await create_chat(current_user["_id"])
         return {"message": "Chat created successfully!", "chat_id": str(chat_id)}
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
 @chats.post("/{chat_id}/send_question")
-async def send_question_route(chat_id: str, question: MessageModel, current_user: dict = Depends(get_current_user)):
+async def send_question_route(
+    chat_id: str, question: MessageModel, current_user: dict = Depends(get_current_user)
+):
     """
     Send a message in a specific chat with provided question and answer.
     Args:
@@ -115,10 +133,12 @@ async def send_question_route(chat_id: str, question: MessageModel, current_user
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail="Internal Server Error")
-    
+
 
 @chats.get("/{chat_id}/history")
-async def show_chat_history(chat_id: str, current_user: dict = Depends(get_current_user)):
+async def show_chat_history(
+    chat_id: str, current_user: dict = Depends(get_current_user)
+):
     """
     Get the chat history for a specific chat ID.
     Args:
@@ -135,17 +155,39 @@ async def show_chat_history(chat_id: str, current_user: dict = Depends(get_curre
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
+
 @main.get("/", include_in_schema=False)
 def get_data():
-    return FileResponse("static/main.html") 
+    return FileResponse("static/main.html")
+
 
 @chats.get("/", include_in_schema=False)
 def get_data():
-    return FileResponse("static/chat.html") 
+    return FileResponse("static/chat.html")
 
 
 @router.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
+    """
+    Uploads a file and processes it based on its content type.
+
+    This endpoint accepts a file upload and processes the file based on its MIME type. It supports processing for PDF, CSV, DOCX, and plain text files. The processed text is then stored in a MongoDB collection, and the function returns the ID of the created database entry along with the file name.
+
+    Args:
+        file (UploadFile): The file to be uploaded. Accepted types are PDF, CSV, DOCX, and plain text.
+
+    Returns:
+        JSONResponse: A response object containing the ID of the created database entry and the file's name. The response status code is set to 200 for a successful upload.
+
+    Raises:
+        HTTPException: If the file type is unsupported or if an error occurs during file processing or database insertion, an HTTPException with status code 400 (for unsupported file type) or 500 (for other errors) is raised.
+
+    Note:
+        The function includes a size check for CSV, DOCX, and plain text files. If the content length exceeds 4000 characters, a response indicating a size warning is returned.
+
+    Example:
+        To use this endpoint, send a POST request with a file to '/upload/'. The response will contain the file's ID in the database and its name.
+    """
     try:
         file_content = await file.read()
 
@@ -189,19 +231,24 @@ async def upload_file(file: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error processing file: {str(e)}")
 
-@users.post('/pay')
+
+@users.post("/pay")
 async def pay(current_user: dict = Depends(get_current_user)):
     result = await get_payment()
     return result
 
 
 @payment.get("/execute")
-async def execute_payment(request: Request, current_user: dict = Depends(get_current_user)):
+async def execute_payment(
+    request: Request, current_user: dict = Depends(get_current_user)
+):
     payment_id = request.query_params.get("paymentId")
     payer_id = request.query_params.get("PayerID")
 
     if payment_id and payer_id:
-        success = await execute_paypal_payment(payment_id, payer_id, current_user.get('username'))
+        success = await execute_paypal_payment(
+            payment_id, payer_id, current_user.get("username")
+        )
 
         if success:
             return {"status": "Payment successful, user upgraded to premium"}
@@ -209,6 +256,3 @@ async def execute_payment(request: Request, current_user: dict = Depends(get_cur
             return {"status": "Payment failed"}
     else:
         return {"status": "Missing paymentId or PayerID"}
-
-    
-
